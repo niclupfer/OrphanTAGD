7e340fe53a8b3daec2ce429a07757ba16aca787f
AUTH\pockolypse1 <paraliticcirnp@gmail.com>
2017-02-11 17:27:19 -0600

    Basic Hunger Bar with food script

---
+++ MiniProjects/ProgressBars2/Assets/Hunger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hunger : MonoBehaviour {
+    static public float hunger = 100;
+    public float hungerRate = 2;
+   // public float timerRate;
+    public float timer = 10.0f;
+
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime; 
+        if(timer < 0)
+        {
+            hunger -= hungerRate;
+            timer = 10.0f;
+        }
+        if(hunger < 1)
+        {
+            hunger = 0;
+        }
+        gameObject.GetComponent<Text>().text = "" + hunger.ToString();
+	}
+}
