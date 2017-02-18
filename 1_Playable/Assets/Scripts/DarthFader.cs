1d0760d4f1b45cd6e4919bbfcec37db5486ceb31
Nic Lupfer <nlupfer@gmail.com>
2017-02-17 18:02:02 -0600

    start of first playable prototype

---
+++ 1_Playable/Assets/Scripts/DarthFader.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DarthFader : MonoBehaviour {
+
+	public float fadeTime;
+	public bool visible = true;
+
+	void Start ()
+	{
+		// full black
+		GetComponent<RawImage> ().color = new Color(0f, 0f, 0f, 1f);
+
+		// fade in finished
+		visible = true;
+	}
+
+	public void FadeOut ()
+	{
+		StartCoroutine (Do_FadeOut());
+	}
+
+	public void FadeIn (StateCallBack callback)
+	{
+		StartCoroutine (Do_FadeIn(callback));
+	}
+
+	IEnumerator Do_FadeIn(StateCallBack callback)
+	{
+		visible = true;
+
+		var doneTime = Time.time + fadeTime;
+
+		while (Time.time < doneTime)
+		{
+			GetComponent<RawImage> ().color = new Color(0f, 0f, 0f, 1f - ((doneTime - Time.time)/fadeTime));
+			yield return new WaitForFixedUpdate();
+		}
+
+		// full black
+		GetComponent<RawImage> ().color = new Color(0f, 0f, 0f, 1f);
+
+		// fade in finished
+		callback();
+
+		yield return null;
+	}
+
+	IEnumerator Do_FadeOut()
+	{
+		var doneTime = Time.time + fadeTime;
+
+		while (Time.time < doneTime)
+		{
+			GetComponent<RawImage> ().color = new Color(0f, 0f, 0f, 0f + ((doneTime - Time.time)/fadeTime));
+			yield return new WaitForFixedUpdate();
+		}
+
+		// full transparent
+		GetComponent<RawImage> ().color = new Color(0f, 0f, 0f, 0f);
+
+		// fade out finished
+		visible = false;
+
+		yield return null;
+	}
+}
---
+++ 1_Playable/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Runtime.InteropServices; 
+
+public enum GAME_STATE { title, howTo, playing, over };
+
+public delegate void StateCallBack(GAME_STATE newState);  
+
+public class GameMaster : MonoBehaviour {
+	
+	public GAME_STATE state;
+
+	DarthFader fader;
+
+	void Start ()
+	{
+		fader = GameObject.Find ("Fader").GetComponent<DarthFader> ();
+
+		state = GAME_STATE.title;
+
+		// fade in title
+		fader.FadeOut();
+
+		// wait for click or key press
+
+		// fade out title
+		// fade in how to play
+
+		// wait for click or key press
+
+		// fade out how to play
+
+		// create city
+			// blocks
+			// buildings
+			// cars
+			// hurdles - trashcans, crates
+			// shops
+				// table + food
+				// radio
+				// baker
+
+		// create and enable player
+
+		// start game stuff
+			// radios play
+			// player gets hungry
+			// bakers ready to chase thiefs
+
+		// wait for player to starve
+
+		// fade out game
+		// fade in game over
+
+		// wait for click or key press
+
+		// fade out game over
+		// fade in title and repeat
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			// quit the game
+			Application.Quit();
+		}
+
+		if (!fader.visible && Input.anyKeyDown)
+		{
+			if (state == Game_STATE.title)
+			{
+				StateCallBack cb = new StateCallBack (GameMaster.StateState (GAME_STATE.howTo));
+				fader.FadeIn (cb);
+			}
+		}
+	}
+
+	public void StartState(GAME_STATE newState)
+	{
+		GameObject.Find("Title").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 0f);
+		GameObject.Find("HowTo").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 0f);
+		GameObject.Find("Title").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 0f);
+
+		switch (newState)
+		{
+			case GAME_STATE.title:
+				GameObject.Find("Title").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 1f);
+				break;
+
+			case GAME_STATE.howTo:
+				GameObject.Find("HowTo").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 1f);
+				break;
+
+			case GAME_STATE.playing:
+				
+				break;
+
+			case GAME_STATE.over:
+				GameObject.Find("Gameover").GetComponent<RawImage>().color = new Color(0f, 0f, 0f, 1f);
+				break;
+		}
+
+		state = newState;
+	}
+}
