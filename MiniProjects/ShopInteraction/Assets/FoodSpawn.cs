c051629bc9d5317c64855eef1b5924b3fb822582
AUTH\edls2013 <edls2013@tamu.edu>
2017-02-18 19:15:43 -0600

    food spawning (not quite working yet)

---
+++ MiniProjects/ShopInteraction/Assets/FoodSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawn : MonoBehaviour {
+
+	public GameObject Food;
+	GameObject FoodClone;
+	int foodCount = 0;
+
+	void Update(){
+		while (foodCount == 0) {
+			FoodClone = Instantiate (Food, transform.position, Quaternion.identity) as GameObject;
+			foodCount = 1;
+			Destroy (FoodClone, 5);
+		}
+	}
+
+	void OnDestroy() {
+		foodCount = 0;
+	}
+
+}
--- MiniProjects/ShopInteraction/Assets/Scripts/LoafPickup.cs
+++ MiniProjects/ShopInteraction/Assets/Scripts/LoafPickup.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class LoafPickup : MonoBehaviour {
 
+	int hungerValue;
 	public bool onTable;
 	public bool inBakerHands;
 	public bool inPlayerHands;
@@ -20,6 +21,7 @@ public class LoafPickup : MonoBehaviour {
 		if (other.gameObject.tag == "Player") {
 			onTable = false;
 			transform.parent = other.transform;
+			inPlayerHands = true;
 		}
 		if (other.gameObject.tag == "Baker"){
 			onTable = false;
@@ -29,17 +31,34 @@ public class LoafPickup : MonoBehaviour {
 
 	void eatItem() {
 		/*insert code for changing hunger before destroying bread loaf*/
+		if(this.tag == "Bread")
+			hungerValue = 25;
+		if(this.tag == "Orange")
+			hungerValue = 10;
+		if(this.tag == "Apple")
+			hungerValue = 10;
+		// Player.replenishHunger(hungerValue);
 		Destroy (gameObject);
 	}
 
+	void OnDestroy(){
+		
+	}
+
 	// Update is called once per frame
-	void Update () {
-		if (onTable == false) {
+	void Update () {	
+		if (inPlayerHands == true) {
 			//gameObject.SetInactive();
 			//GetComponent<Renderer>().enabled = false;
+			//if (player presses button to eat item)
+			isEaten = true;
+		
+			if (isEaten == true) {
+				eatItem ();
+			}
 		}
-		if (isEaten == true) {
-			eatItem ();
+		if(inBakerHands == true){
+			// make food item able to be stolen
 		}
 	}
 }
