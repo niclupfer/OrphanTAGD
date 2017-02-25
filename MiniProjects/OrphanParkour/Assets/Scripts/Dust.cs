686f54f9a2853f1a09619434f2384cd2af72d6ed
niclupfer <nlupfer@gmail.com>
2017-02-25 13:05:05 -0600

    parkour!

---
+++ MiniProjects/OrphanParkour/Assets/Scripts/Dust.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dust : MonoBehaviour {
+
+    float startSize;
+    public float maxSize;
+    public float upSpeed;
+    public float lifeTime;
+
+    float birthTime;
+    void Start ()
+    {
+        startSize = transform.localScale.x;
+        birthTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        // gets bigger
+        var newScale = Mathf.Lerp(startSize, maxSize, (Time.time -birthTime) / lifeTime);
+        transform.localScale = new Vector3(newScale, newScale, newScale);
+
+        // moves up
+        transform.position += new Vector3(0f, upSpeed * Time.deltaTime, 0f);
+        transform.forward = -Camera.main.transform.forward;
+
+        // fades out
+        var newColor = new Color(1f, 1f, 1f, 0.6f - (Time.time - birthTime) / lifeTime);
+        GetComponent<SpriteRenderer>().color = newColor;
+
+        // dies
+        if(Time.time > birthTime + lifeTime)
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
