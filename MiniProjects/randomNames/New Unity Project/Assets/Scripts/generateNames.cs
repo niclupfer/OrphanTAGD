879742a83da0d8d3abb14f4d3daefd5eeefa41f4
Jasmine Derry <jazzyd14@tamu.edu>
2017-02-26 18:25:33 -0600

    Creating the asset and testing Unity’s Random class

---
+++ MiniProjects/randomNames/New Unity Project/Assets/Scripts/generateNames.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class generateNames : MonoBehaviour {
+
+	public Button nameButton;
+	public string[] stringArray;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = nameButton.GetComponent<Button>();
+		btn.onClick.AddListener(printName);
+		for(int i = 0; i < 10; i++){
+			stringArray[i] = ("Hello!"+i);
+		}
+	}
+
+	void printName() {
+		Debug.Log(stringArray[Random.Range(0,9)]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
