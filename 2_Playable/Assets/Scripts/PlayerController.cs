f4ba1a4cc53056c7c5a570552310a27af118edc0
Nic <Nic@rothko.cs.tamu.edu>
2017-03-16 18:16:09 -0500

    more sounds, better footstep timing, and vaulting

--- 2_Playable/Assets/Scripts/PlayerController.cs
+++ 2_Playable/Assets/Scripts/PlayerController.cs
@@ -34,8 +34,8 @@ public class PlayerController : MonoBehaviour {
     public bool onGround = false;
 
     public GameObject dustObj;
-    public float dustInterval;
-    float lastDustTime;
+    public float footstepMod;
+    float lastFootstepTime;
 
     public float jumpSpeed;
     public float jumpTime;
@@ -46,13 +46,25 @@ public class PlayerController : MonoBehaviour {
 
 	public float rollSpeed;
 	public float rollTime;
-	public float rollShrinkDelay;
+	public float[] rollDownTimes;
+	public float[] rollUpTimes;
 	Vector3 rollDirection;
 	public float forwards = 0f;
+
+	Vector3 lastPosition;
+	float lastDelta;
+	float clickTimeout = 0.3f;
+	float lastSideMoveTime;
+	bool wasLeft;
+	bool wasRight;
+
     /*
     public GameObject eatSoundObj;
     */
     public AudioClip footstep;
+	public AudioClip jump;
+	public AudioClip hup;
+	public AudioClip roll;
     public AudioManager am;
 
     public Transform rightHand;
@@ -92,6 +104,33 @@ public class PlayerController : MonoBehaviour {
 
         moveSideways = Input.GetAxis("Horizontal");
         moveForward = Input.GetAxis("Vertical");
+
+		/*
+		if (moveSideways > 0)
+		{
+			if (lastSideMoveTime + clickTimeout > Time.time && wasRight == true && CanJump ()) {
+				// do right juke;
+				anim.SetTrigger("Juke");
+			}			
+
+			wasRight = true;
+			wasLeft = false;
+		}
+		else if (moveSideways < 0)
+		{
+			if (lastSideMoveTime + clickTimeout > Time.time && wasLeft == true && CanJump ()) {
+				// do left juke;
+				anim.SetTrigger("Juke");
+			}	
+			wasLeft = true;
+			wasRight = false;
+		}
+		else
+		{
+			lastSideMoveTime = Time.time;
+		}
+		*/
+
 
         if(moveSideways != 0 || moveForward != 0)
         {
@@ -163,11 +202,6 @@ public class PlayerController : MonoBehaviour {
 			StartCoroutine (DoRoll ());            
         }
     
-
-
-        if ((moveForward != 0 || moveSideways != 0) && lastDustTime + (dustInterval/speedMod) < Time.time)
-            MakeDust();
-
         /*
         if(loafInHands == true)
         {
@@ -271,13 +305,31 @@ public class PlayerController : MonoBehaviour {
 		anim.ResetTrigger("Jump"); 
 		anim.SetTrigger("Jump");
 
-		crouching = false;
-		anim.SetBool ("Crouching", crouching);
+		anim.ResetTrigger("Cool");
+		anim.ResetTrigger("Vault");
+
 		if (speedMod < 1f)
 			speedMod = 1f;
 
+		// if moving towards a vaultable object at the right distance
+		if (!crouching && ObjectInFront_Vaultable ()) {
+			// do cool animation and give speed boost
+			anim.SetTrigger ("Vault");
+			//speedMod += 0.3f;
+			// notify player of coolness somehow
+		}
+
+		if (sprinting) {
+			if (Random.Range (0, 10) > 5)
+				anim.SetTrigger ("Cool");
+		}
+
+		crouching = false;
+		anim.SetBool ("Crouching", crouching);
+
+
 		if (Mathf.Abs(moveForward) + Mathf.Abs(moveSideways) > 0.1f) {
-			if(speedMod > 1f)
+			if(speedMod > 1.3f)
 				yield return new WaitForSeconds(jumpDelay_sprinting);
 			else {
 				yield return new WaitForSeconds(jumpDelay_running);
@@ -290,13 +342,19 @@ public class PlayerController : MonoBehaviour {
         var standardHeight = GetComponent<CapsuleCollider>().height;
         GetComponent<CapsuleCollider>().height = 2f;
 
+		am.PlaySFX(jump, 1, true);
+
         var jumpEndTime = Time.time + jumpTime;
         yield return new WaitForFixedUpdate();
+
+		am.PlaySFX(hup, 1, true);
 
         while(Time.time < jumpEndTime)
         {
             ups = Mathf.Lerp(jumpSpeed, 0, 1-(jumpEndTime - Time.time ) / jumpTime);
             yield return new WaitForFixedUpdate();
+
+
         }
 
         //yield return new WaitForSeconds(jumpTime);
@@ -307,6 +365,8 @@ public class PlayerController : MonoBehaviour {
         GetComponent<CapsuleCollider>().height = standardHeight;
         ups = 0f;
         anim.ResetTrigger("Jump");
+		anim.ResetTrigger("Cool");
+		anim.ResetTrigger("Vault");
     }
 
 	IEnumerator DoRoll()
@@ -319,20 +379,30 @@ public class PlayerController : MonoBehaviour {
 		if (speedMod < 1f)
 			speedMod = 1f;
 
+		am.PlaySFX(roll, 1, true);
+
 		rollDirection = transform.forward;
 		forwards = rollSpeed;
 	
 		var rollEndTime = Time.time + rollTime;
-		yield return new WaitForFixedUpdate();
-
-		yield return new WaitForSeconds (rollShrinkDelay);
+		var rollDownTime = Time.time + rollDownTimes [0];
+		var rollUpTime = Time.time + rollUpTimes [0];
 
 		var standardHeight = GetComponent<CapsuleCollider>().height;
-		GetComponent<CapsuleCollider>().height = 2f;
 
-		yield return new WaitForSeconds(rollTime - rollShrinkDelay);
+		while(Time.time < rollEndTime)
+		{
+			if( Time.time > rollDownTime)
+				GetComponent<CapsuleCollider>().height = 2f;
+
+			if(Time.time > rollUpTime)
+				GetComponent<CapsuleCollider>().height = standardHeight;
+
+			yield return new WaitForFixedUpdate();
+		}
 
 		GetComponent<CapsuleCollider>().height = standardHeight;
+
 		forwards = 0f;
 		anim.ResetTrigger("Roll");
 	}
@@ -386,7 +456,8 @@ public class PlayerController : MonoBehaviour {
         //Debug.Log("normalized : " + (moveF + moveS).normalized);
         var moveMag = (moveF + moveS).magnitude;
         moveMag = Mathf.Clamp(moveMag, -1f, 1f);
-        anim.SetFloat ("Speed", moveMag * speed * speedMod * Time.deltaTime);
+		var actualSpeed = moveMag * speed * speedMod * Time.deltaTime;
+		anim.SetFloat ("Speed", actualSpeed);
 
         //rb.velocity = (moveF + moveS).normalized * speed * speedMod * Time.deltaTime;
         //rb.velocity += new Vector3(0f, ups, 0f);
@@ -404,334 +475,54 @@ public class PlayerController : MonoBehaviour {
 
         rb.AddForce(targetSpeed - rb.velocity, ForceMode.VelocityChange);
 
-        //var goodCollisionPos = Move(
-        //    targetSpeed,
-        //    transform.position);
+ 		// check for footstep
+		if (actualSpeed > 0f && onGround && forwards == 0f) {
+			//Debug.Log (footstepMod / actualSpeed);
+			if (footstepMod / actualSpeed < Time.time - lastFootstepTime)
+				Footstep ();
+		}
 
-        //transform.position = goodCollisionPos;
-        //controller.Move((moveF + moveS).normalized * speed * speedMod * Time.deltaTime);
+		var delta = (transform.position - lastPosition).magnitude;
+		Debug.Log ("Delta: " + delta);
+		if (delta < 0.05f && sprinting && lastDelta > 0.15f) {
+			// must have run into something
+			anim.SetTrigger("Ran Into Something");
+		}
 
+		lastDelta = delta;
+		lastPosition = transform.position;
+    }
+
+	void Footstep()
+    {
+		lastFootstepTime = Time.time;
+
+		// make dust
+		if (!crouching)
+		{
+			var dust = Instantiate (dustObj);
+			dust.transform.position = transform.position;
+		}
         
+		// play footstep sound
+		var volume = 1f;
+		if (crouching)
+			volume = 0.3f;
+		
+		am.PlaySFX(footstep, volume, true);
     }
 
-    void MakeDust()
-    {
-        var dust = Instantiate(dustObj);
-        dust.transform.position = transform.position;
-        lastDustTime = Time.time;
+	bool ObjectInFront_Vaultable()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position + (transform.up * 1f), transform.forward, out hit, 3f * speedMod)) {
+			Debug.Log ("Front Hit " + hit.distance);
+			if(hit.game hit.distance > 2f)
+				return true;
+		}
 
-        //am.PlaySFX(footstep, 1, true);
-    }
-
-    public Vector3 Move(Vector3 moveAmount, Vector3 position)
-    {
-        float deltaY = moveAmount.y;
-
-        //Vector3 entityPosition = position;
-        // Resolve any possible collisions below and above the entity.
-        //deltaY = yAxisCollisions(deltaY, entityPosition);
-
-        // Resolve any possible collisions left and right of the entity.
-        // Check if our deltaX value is 0 to avoid unnecessary collision detection.
-        if (moveAmount.x != 0 || moveAmount.z != 0)
-        {
-            moveAmount = xzAxisCollisions(new Vector3(moveAmount.x, 0f, moveAmount.z), position);
-        }        
-
-        Vector3 finalTransform = position + new Vector3(moveAmount.x, 0f, moveAmount.z);
-
-        return finalTransform;
-    }
-
-    public Vector3 enPosition;
-    public Vector3 offsetPos;
-    public Vector3 centerPos;
-    public float radius;
-
-    private Vector3 xzAxisCollisions(Vector3 xzVec, Vector3 entityPosition)
-    {
-        var cap = GetComponent<CapsuleCollider>();
-        
-        var offset = new Vector3((xzVec.normalized.x * cap.radius), 0f, (xzVec.normalized.z * cap.radius));
-        var centerPoint = entityPosition + offset;
-        centerPoint = new Vector3(centerPoint.x, cap.center.y, centerPoint.z);
-
-
-        //var target = new Vector3(xzVec.x, 0, xzVec.z);
-        RaycastHit hit;
-        if (Physics.Raycast(centerPoint, transform.forward, xzVec.magnitude))
-        {
-            Debug.DrawRay(transform.position, transform.forward, Color.red);
-            // print("There is something in front of the object!");
-            xzVec = Vector3.zero;
-        }
-
-        return new Vector3(xzVec.x, 0f, xzVec.z);
-
-        /*
-        Physics.Raycast(centerPoint, xzVec.normalized, out hit, xzVec.magnitude, collisionMask);
-
-        Debug.DrawRay(centerPoint, xzVec, Color.yellow);
-        //Debug.DrawLine(centerPoint, Vector3.zero, Color.green);
-        enPosition = entityPosition;
-        offsetPos = offset;
-        centerPos = centerPoint;
-        radius = cap.radius;
-        
-        if (hit.collider)
-        {
-            Debug.Log("Hit");
-            Debug.DrawLine(centerPoint, centerPoint + xzVec, Color.red);
-            float distance = hit.distance;
-            // Stop entity's downward movement after coming within skin width of a boxCollider
-            if (distance > 0.005f)
-            {
-                xzVec = xzVec.normalized * (cap.radius + 0.005f);
-            }
-            else
-            {
-                xzVec = Vector3.zero;
-            }
-        }
-        else
-        {
-            Debug.DrawLine(centerPoint, centerPoint + xzVec, Color.cyan);
-        }
-        
-
-        return new Vector3(xzVec.x, 0f, xzVec.z);
-
-        /*
-        // center points
-        for (var i = 0; i < 4; i++)
-        {
-            float dirX = Mathf.Sign(deltaX);
-
-            float x = entityPosition.x + cap.center.x + cap.radius / 2 * dirX;
-            float y = (entityPosition.y + cap.center.y - cap.height / 2) + cap.height / 2 * i;
-
-            RaycastHit2D hit;
-            hit = Physics2D.Raycast(new Vector2(x, y), new Vector2(dirX, 0), Mathf.Abs(deltaX), collisionMask);
-
-            Debug.DrawRay(new Vector3(x, y, 0), new Vector3(dirX, 0, 0), Color.cyan);
-
-            if (hit)
-            {
-                if (hit.collider.gameObject.tag == "Ground")
-                {
-                    //Debug.DrawRay(new Vector2(x, y), hit.point, Color.red);
-                    float distance = hit.distance;
-                    // Stop entity's downward movement after coming within skin width of a boxCollider
-                    if (distance > 0.005f)
-                    {
-                        deltaX = distance * dirX + 0.005f;
-                    }
-                    else
-                    {
-                        deltaX = 0;
-                    }
-                    //SideCollision = true;
-
-                    break;
-                }
-                else if (hit.collider.gameObject.tag == "Spikes")
-                {
-                    //GetHit(hit.collider.gameObject);
-
-                    break;
-                }
-                else if (hit.collider.gameObject.tag == "Coin")
-                {
-                    //PickupCoin(hit.collider.gameObject);
-                    break;
-                }
-            }
-        }
-        
-        return deltaX;
-        */
-    }
-
-    /*
-    private float yAxisCollisions(float deltaY, float dirX, Vector3 entityPosition)
-    {
-        OnGround = false;
-        // To prevent falling through collision layers by a gap in the corner
-        // if we are facing right, peform y-axis raycasts starting on the right.
-        int facingRight = 1;
-        if (dirX == facingRight)
-        {
-            for (int i = 2; i > -1; --i)
-            {
-                if (yAxisRaycasts(i, ref deltaY, entityPosition))
-                {
-                    break;
-                }
-            }
-            // else we are facing left, peform y-axis raycasts starting on the left
-        }
-        else
-        {
-            for (int i = 0; i < 3; ++i)
-            {
-                if (yAxisRaycasts(i, ref deltaY, entityPosition))
-                {
-                    break;
-                }
-            }
-        }
-
-        return deltaY;
-    }
-
-    private bool yAxisRaycasts(int i, ref float deltaY, Vector3 entityPosition)
-    {
-        float dirY = Mathf.Sign(deltaY);
-        //Debug.Log("i: " + i);
-        // Start at the left or the right of the boxCollider, depending on the value of i.
-        float x = (entityPosition.x + offset.x - size.x / 2) + (size.x / 2) * i;
-
-        if (i == 0)
-            x += 1f;
-        else if (i == 2)
-            x -= 1f;
-
-        // Bottom or top of boxCollider, depending on if dirY is positive or negative
-        float y = entityPosition.y + offset.y + size.y / 2 * dirY;
-
-        RaycastHit2D hit;
-        //Physics2D.Raycast()
-        hit = Physics2D.Raycast(new Vector2(x, y), new Vector2(0, dirY), Mathf.Abs(deltaY), collisionMask);
-
-        //Debug.DrawRay(new Vector2(x, y), new Vector2(0, dirY), Color.cyan);
-
-        if (hit)
-        {
-            //Debug.DrawRay(new Vector2(x, y), hit.point, Color.red);
-            if (hit.collider.gameObject.tag == "Ground")
-            {
-                // Get Distance between entity and ground
-                float distance = hit.distance;
-                // Stop entity's downward movement after coming within skin width of a boxCollider
-                if (distance > skin)
-                {
-                    deltaY = distance * dirY + skin;
-                }
-                else
-                {
-                    deltaY = 0;
-                }
-                if (dirY < 0)
-                    OnGround = true;
-                else if (jumping)
-                    StopJump();
-                else if (flying)
-                {
-                    Instantiate(landSoundObj);
-                    StopFlying();
-                }
-
-                return true;
-            }
-            else if (hit.collider.gameObject.tag == "JumpThru" && dirY < 0 && !squatting)
-            {
-                // Get Distance between entity and ground
-                float distance = hit.distance;
-                // Stop entity's downward movement after coming within skin width of a boxCollider
-                if (distance > skin)
-                {
-                    deltaY = distance * dirY + skin;
-                }
-                else
-                {
-                    deltaY = 0;
-                }
-                OnGround = true;
-
-                return true;
-            }
-            else if (hit.collider.gameObject.tag == "Spikes")
-            {
-                GetHit(hit.collider.gameObject);
-            }
-            else if (hit.collider.gameObject.tag == "Coin")
-            {
-                PickupCoin(hit.collider.gameObject);
-
-            }
-        }
-
-        return false;
-    }
-    */
-
-    /*
-    public void DieOfStarvation()
-    {
-        if (!dead)
-        {
-            dead = true;
-            HideAllText();
-            loafInHands = false;
-            if (foodInHands != null)
-                Destroy(foodInHands);
-
-            // game over blargh!
-            //GameObject.Find("Fader").GetComponent<DarthFader>().FadeIn(GAME_STATE.over);
-            GameObject.Find("GameMaster").GetComponent<GameMaster>().StartState(GAME_STATE.over);
-        }
-    }
-
-    void HideAllText()
-    {
-        GameObject.Find("StealText").GetComponent<Text>().color = new Color(0f, 0f, 0f, 0f);
-        GameObject.Find("EatText").GetComponent<Text>().color = new Color(0f, 0f, 0f, 0f);
-    }
-
-    void ShowEatText()
-    {
-        HideAllText();
-        GameObject.Find("EatText").GetComponent<Text>().color = new Color(0f, 0f, 0f, 1f);
-    }
-
-    void ShowStealText()
-    {
-        HideAllText();
-        GameObject.Find("StealText").GetComponent<Text>().color = new Color(0f, 0f, 0f, 1f);
-    }
- 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "ShopTable")
-        {
-            if (other.GetComponent<ShopTable>().HasFood())
-            {
-                closestTable = other.gameObject;
-                nearTable = true;
-            }
-            else
-                nearTable = false;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "ShopTable")
-        {
-            nearTable = false;
-        }
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.collider.tag == "ShopKeeper")
-        {
-            if (other.collider.GetComponent<BakerAI>().chasing)
-            {
-                // you got caught
-                DieOfStarvation();
-            }
-        }
-    }
-    */
+		
+		return false;
+	}
+    
 }
