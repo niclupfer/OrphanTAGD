bef42382f432b2f9078c106bf56c494b46b9319e
rvalde28 <rvalde28@tamu.edu>
2017-04-08 16:28:39 -0500

    Add files via upload

--- MiniProjects/DayNightTest/Assets/Scripts/StreetLight.cs
+++ MiniProjects/DayNightTest/Assets/Scripts/StreetLight.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StreetLight : MonoBehaviour {
-	Light mainLight;
-	public float maxIntensity;
-	public float minIntensity;
-	// Use this for initialization
-	void Start () {
-		mainLight = GetComponent<Light> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float range;
-		float i = (((maxIntensity - minIntensity)*range) + minIntensity);
-		mainLight.intensity = i;
-	}
+public class StreetLight : MonoBehaviour
+{
+    Light mainLight;
+    public float maxIntensity = 1.6f;
+    public float minIntensity = 0.5f;
+
+    [HideInInspector]
+    public float lightDelay = 0.0f;
+    public float maxTime = 0.09f;
+    
+    // Use this for initialization
+    void Start()
+    {
+        //StartCoroutine(updateLight());
+        mainLight = GetComponent<Light>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightDelay > maxTime)
+        {
+            lightDelay = 0;
+            float i = Random.Range(minIntensity, maxIntensity);
+            mainLight.intensity = i;
+        }
+        lightDelay += Time.deltaTime;
+    }
+
 }
+
